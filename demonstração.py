ec1efc4389e2d72446e13f92dea1c45412bbff5d
thiagov21 <77083955+thiagov21@users.noreply.github.com>
2021-01-06 22:30:39 -0300

    Add files via upload

---
+++ demonstração.py
@@ -0,0 +1,16 @@
+horario = 'noite'
+
+if horario == 'dia':
+  print('O Sol esta quente')
+elif horario == 'tarde':
+  print('O por do sol está chegando')
+else:
+  print('O por do sol está chegando')
+
+
+temperatura = 22
+
+if temperatura <= 39:
+  print('Não esta tão quente')
+else:
+    print('Está muito quente!')
